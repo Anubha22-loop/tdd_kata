7203cc7dff1870621a469f92730bef482c6d04cb
“Anubha” <anubha.gupta@finaxar.com>
2024-04-03 13:45:42 +0530

    refactored code to add method to get number arr

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,33 +1,43 @@
 describe StringCalculator do
-  let!(:str_calculator) { described_class.new }
   describe '#add' do
     it 'should return 0 for empty string' do
-      expect(str_calculator.add('')).to eq(0)
+      str_calculator = described_class.new
+      expect(str_calculator.add).to eq(0)
     end
 
     context 'single number' do
       it 'return 0 for 0' do
-        expect(str_calculator.add('0')).to eq(0)
+        str_calculator = described_class.new('0')
+        expect(str_calculator.add).to eq(0)
       end
 
       it 'return 78 for 78' do
-        expect(str_calculator.add('78')).to eq(78)
+        str_calculator = described_class.new('78')
+        expect(str_calculator.add).to eq(78)
       end
     end
 
     context '2 numbers' do
       it 'should return 6 for 1,5' do
-        expect(str_calculator.add('1,5')).to eq(6)
+        str_calculator = described_class.new('1,5')
+        expect(str_calculator.add).to eq(6)
       end
 
       it 'should return 30 for 20,10' do
-        expect(str_calculator.add('20,10')).to eq(30)
+        str_calculator = described_class.new('20, 10')
+        expect(str_calculator.add).to eq(30)
       end
     end
 
     context '3 numbers' do
       it 'should return 6 for 1,2,3' do
-        expect(str_calculator.add('1,2,3')).to eq(6)
+        str_calculator = described_class.new('1,2,3')
+        expect(str_calculator.add).to eq(6)
+      end
+
+      it 'should return 100 for 50,25,25' do
+        str_calculator = described_class.new('50,25,25')
+        expect(str_calculator.add).to eq(100)
       end
     end
   end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,9 +1,15 @@
 class StringCalculator
-  def add(numbers = '')
-    return 0 if numbers.empty?
-    return numbers.to_i unless numbers.include?(',')
-    number_arr = numbers.split(',').map {|n| n.to_i}
-    return number_arr[0] + number_arr[-1] if number_arr.length == 2
-    number_arr[0] + number_arr[1] + number_arr[2]
+
+  def initialize(numbers = '')
+    @numbers = numbers
+  end
+
+  def add
+    return 0 if @numbers.empty?
+    number_arr.sum
+  end
+
+  def number_arr
+    @numbers.split(',').map {|n| n.to_i}
   end
 end
