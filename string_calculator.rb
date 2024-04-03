19238b3db957402f22f6a26f5dc77310de2ed6cd
“Anubha” <anubha.gupta@finaxar.com>
2024-04-03 13:33:15 +0530

    refactored code for 2 numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,7 @@ class StringCalculator
   def add(numbers = '')
     return 0 if numbers.empty?
     return numbers.to_i unless numbers.include?(',')
-    number_arr = numbers.split(',')
-    number_arr[0].to_i + number_arr[-1].to_i
+    number_arr = numbers.split(',').map {|n| n.to_i}
+    number_arr[0] + number_arr[-1]
   end
 end
