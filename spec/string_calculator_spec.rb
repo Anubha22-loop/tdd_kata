afe1f1eccc3e6abeb34683440db6bf6abfb4ccde
“Anubha” <anubha.gupta@finaxar.com>
2024-04-03 13:19:11 +0530

    added test for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,2 +1,8 @@
 describe StringCalculator do
+  let!(:str_calculator) { described_class.new }
+  describe '#add' do
+    it 'should return 0 for empty string' do
+      expect(str_calculator.add('')).to eq(0)
+    end
+  end
 end
