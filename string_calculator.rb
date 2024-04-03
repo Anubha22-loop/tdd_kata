85a256f1299618a6f94316b746d9f6e1cada955a
“Anubha” <anubha.gupta@finaxar.com>
2024-04-03 14:34:36 +0530

    corrected method scope and changed method

--- string_calculator.rb
+++ string_calculator.rb
@@ -8,12 +8,14 @@ class StringCalculator
 
   def add
     return 0 if @numbers.empty?
-    raise_error_negative_present?
+    raise_error_negative_present
     number_arr.sum
   end
 
-  def raise_error_negative_present?
-    negative_nums = number_arr.select{ |n| n < 0}
+  private
+
+  def raise_error_negative_present
+    negative_nums = number_arr.select{ |n| n.negative?}
     raise StandardError, "Negative numbers not allowed: #{negative_nums.join(', ')}" unless negative_nums.empty?
   end
 
