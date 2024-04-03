9450d4e61769b29c039794f0d4092e2bee854be0
“Anubha” <anubha.gupta@finaxar.com>
2024-04-03 13:24:17 +0530

    added test for single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,5 +4,15 @@ describe StringCalculator do
     it 'should return 0 for empty string' do
       expect(str_calculator.add('')).to eq(0)
     end
+
+    context 'single number' do
+      it 'return 0 for 0' do
+        expect(str_calculator.add('0')).to eq(0)
+      end
+
+      it 'return 78 for 78' do
+        expect(str_calculator.add('78')).to eq(78)
+      end
+    end
   end
 end
